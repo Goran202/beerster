f2fa20d147f58c0c2eee79393396ed0a5efb6ca1
unknown <gljaljic@gmail.com>
2019-11-04 18:09:44 +0100

    Minor work done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
 class App extends Component {
   state = {};
