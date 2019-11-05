2b61b06161b5e19f7131d6740a23961442c9e0eb
Goran Ljaljić <gljaljic@gmail.com>
2019-11-05 17:40:19 +0100

    Punk API connected

--- src/App.js
+++ src/App.js
@@ -6,26 +6,49 @@ import Content from './components/Content';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Modal from './components/Modal';
 import Join from './components/Join';
+import punkApi from './apis/punkApi';
 
 class App extends Component {
-  state = {};
+  state = {
+    beerList: [],
+    favBeerList: [],
+  };
+
+  componentDidMount() {
+    punkApi
+      .get('beers')
+      .then((response) => {
+        console.log(response);
+        this.setState({ beerList: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    // const local = localStorage.getItem('myListLocal');
+    // if (local) {
+    //   const localJSON = JSON.parse(local);
+    //   this.setState({
+    //     myList: localJSON,
+    //   });
+    // }
+  }
+
   render() {
     return (
       <>
         <Router>
           <Header />
           <Modal type={'exampleModal'} />
+          <Jumbotron />
           <Switch>
             <Route exact path="/">
-              <Jumbotron />
               <Content title="Beer" />
             </Route>
             <Route exact path="/home">
-              <Jumbotron />
               <Content title="Beer" />
             </Route>
             <Route exact path="/favorites">
-              <Jumbotron />
               <Content title="My Favourite beers" />
             </Route>
             <Route exact path="/join">
