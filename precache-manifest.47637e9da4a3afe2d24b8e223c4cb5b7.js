199aecc63e023c62ddaad66286e00481b799919e
Goran Ljaljić <gljaljic@gmail.com>
2019-11-06 13:26:14 +0100

    Updates

---
+++ precache-manifest.47637e9da4a3afe2d24b8e223c4cb5b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3fe285601364f5eea02e276ae77f0e",
+    "url": "/beerster/index.html"
+  },
+  {
+    "revision": "e84f9d4163d847dcc7dc",
+    "url": "/beerster/static/css/main.2969940b.chunk.css"
+  },
+  {
+    "revision": "ca7d3e0a8a852a585144",
+    "url": "/beerster/static/js/2.33e8d452.chunk.js"
+  },
+  {
+    "revision": "e84f9d4163d847dcc7dc",
+    "url": "/beerster/static/js/main.d81f4d21.chunk.js"
+  },
+  {
+    "revision": "c07621cbb249eacc91fa",
+    "url": "/beerster/static/js/runtime-main.77e053f4.js"
+  },
+  {
+    "revision": "98a9943854c22f56131ae9fcd596bf80",
+    "url": "/beerster/static/media/illustration.98a99438.png"
+  },
+  {
+    "revision": "1559ddaa64a06ae0e55a1a2efa4b8e2c",
+    "url": "/beerster/static/media/pattern.1559ddaa.svg"
+  },
+  {
+    "revision": "a217475e9b27c84b08d01d08c98f532a",
+    "url": "/beerster/static/media/pattern.a217475e.png"
+  },
+  {
+    "revision": "bcd854baacb8e502e06e011fb41fb4a9",
+    "url": "/beerster/static/media/pattern_background.bcd854ba.svg"
+  }
+]);
